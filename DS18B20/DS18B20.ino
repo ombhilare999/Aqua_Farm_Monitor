87f71a345e9c982ca7a4cef77c3ffcbcebe0d4e1
ombhilare999 <ombhilare999@gmail.com>
2021-03-07 16:58:12 +0530

    DS18B20 interface added

---
+++ DS18B20/DS18B20.ino
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////
+//  Aqua Farm Monitoring System
+//  Group 2:
+//      - Omkar Bhilare
+//      - Omkar Sargar
+//      - Lukesh Ankawar
+//      - Om Fuke 
+////////////////////////////////////////////////////
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <BlynkSimpleStream.h>
+
+#define BLYNK_PRINT Serial
+#define ONE_WIRE_BUS 2          // Data wire is plugged into digital pin 2 on the Arduino
+
+float sensorData;
+
+char auth[] = "T_NiFIv-ulqUcA4vrA54FE2BcOni1Isa";
+
+OneWire oneWire(ONE_WIRE_BUS);          // Setup a oneWire instance to communicate with any OneWire device
+DallasTemperature sensor(&oneWire);    // Pass oneWire reference to DallasTemperature library
+
+BlynkTimer timer; // Announcing the timer
+
+
+void myTimerEvent()
+{
+  sensorData = sensor.getTempCByIndex(0);
+  Blynk.virtualWrite(V5, sensorData);
+}
+
+void setup(void)
+{
+  sensor.begin();  // Start up the library
+  
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+  
+ timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop(void)
+{ 
+  sensor.requestTemperatures(); // Send the command to get temperatures
+  
+  Blynk.run();
+  timer.run(); // running timer every second
+  
+  delay(50);
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Aqua_Farm_Monitor
-ESP32 Based IOT system to monitor Aqua Farming
+IOT system to monitor Aqua Farming
+
+
+## DS18B20 Temperature Sensor to Arduino
+
+### Pin out of DS18B20:
+
+<p align="center">
+    <img width="508" height="305" src="assets/pinout.png">
+</p>
+
+### Circuit Diagram:
+
+<p align="center">
+    <img width="526" height="324" src="assets/diagram.png">
+</p>
+
+### Output:
+
+<p align="center">
+    <img width="720" height="480" src="assets/output.jpg">
+</p>
