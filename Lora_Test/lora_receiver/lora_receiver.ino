b7d4a2f5fb6f57bde8ba740e1b50d5e735847ad5
ombhilare999 <ombhilare999@gmail.com>
2021-05-02 12:25:09 +0530

    Basic Lora Test Added

---
+++ Lora_Test/lora_receiver/lora_receiver.ino
@@ -0,0 +1,64 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 23
+#define SS 18
+#define RST 14
+#define DIO0 2
+#define BAND 866E6
+
+//packet counter
+int readingID = 0;
+int counter = 0;
+String LoRaMessage = "";
+
+//Initialize LoRa module
+void startLoRA(){
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  while (!LoRa.begin(866E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  startLoRA();
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
